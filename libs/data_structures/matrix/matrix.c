5951be4427973cbdf4b9fc2045eca407efb7e2f1
Stas <loktiost@gmail.com>
2024-03-15 09:20:24 +0300

    memory

--- libs/data_structures/matrix/matrix.c
+++ libs/data_structures/matrix/matrix.c
@@ -5,5 +5,33 @@
 #include <stdbool.h>
 #include <malloc.h>
 # include "../../algorithms/array/array.h"
+typedef struct matrix {
+    int **values; // элементы матрицы
+    int nRows; // количество рядов
+    int nCols; // количество столбцов
+} matrix;
+typedef struct position {
+    int rowIndex;
+    int colIndex;
+} position;
 
+matrix getMemMatrix(int nRows, int nCols) {
+        int **values = (int **) malloc(sizeof(int*) * nRows);
+        for (int i = 0; i < nRows; i++)
+            values[i] = (int *) malloc(sizeof(int) * nCols);
+        return (matrix){values, nRows, nCols};
+    }
+matrix *getMemArrayOfMatrices(int nMatrices,int nRows, int nCols) {
+    matrix *ms = (matrix*) malloc(sizeof(matrix) * nMatrices);
+    for (int i = 0; i < nMatrices; i++)
+        ms[i] = getMemMatrix(nRows, nCols);
+    return ms;
+}
+void freeMemMatrix(matrix *m){
+    free(&m);
+}
+void freeMemMatrices(matrix *ms, int nMatrices){
+    for (int i = 0; i < nMatrices; ++i) {
+        freeMemMatrix(ms+i);
+    }
 #endif
--- libs/data_structures/matrix/matrix.h
+++ libs/data_structures/matrix/matrix.h
@@ -4,5 +4,22 @@
 
 #ifndef UNTITLED7_MATRIX_H
 #define UNTITLED7_MATRIX_H
+typedef struct matrix {
+    int **values; // элементы матрицы
+    int nRows; // количество рядов
+    int nCols; // количество столбцов
+} matrix;
+typedef struct position {
+    int rowIndex;
+    int colIndex;
+} position;
+
+matrix getMemMatrix(int nRows, int nCols);//размещает в динамической памяти матрицу размером nRows на nCols
+
+matrix *getMemArrayOfMatrices(int nMatrices,int nRows, int nCols);//размещает в динамической памяти массив из nMatrices матриц размером nRows на nCols
+
+void freeMemMatrix(matrix *m);//освобождает память, выделенную под хранение матрицы
+
+void freeMemMatrices(matrix *ms, int nMatrices);//освобождает память, выделенную под хранение массива ms из nMatrices матриц
 
 #endif //UNTITLED7_MATRIX_H
