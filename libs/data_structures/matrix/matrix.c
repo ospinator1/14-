6fcb028f73e0613a7b15760d002b8d2995fa9ca1
Stas <loktiost@gmail.com>
2024-03-15 09:07:53 +0300

    first commit matrix

---
+++ libs/data_structures/matrix/matrix.c
@@ -0,0 +1,9 @@
+
+#ifndef UNTITLED7_MATRIX_С
+#define UNTITLED7_MATRIX_С
+#include <stdio.h>
+#include <stdbool.h>
+#include <malloc.h>
+# include "../../algorithms/array/array.h"
+
+#endif
---
+++ libs/data_structures/matrix/matrix.h
@@ -0,0 +1,8 @@
+//
+// Created by Assa on 05.03.2024.
+//
+
+#ifndef UNTITLED7_MATRIX_H
+#define UNTITLED7_MATRIX_H
+
+#endif //UNTITLED7_MATRIX_H
