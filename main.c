1b793ab4168cc1f2a4b272ae20e1c317e3b85caa
Stas <loktiost@gmail.com>
2024-03-30 10:33:13 +0300

    11 задача

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include <assert.h>
 #include <math.h>
 #include "libs/algorithms/array/array.h"
+
 int getMaxElementRow(matrix m) {
     int max = m.values[0][0];
     int row = 0;
@@ -34,35 +35,36 @@ int getMinElementRow(matrix m) {
     return row;
 }
 
-int getMax(int *a, int n){
-    int max=0;
+int getMax(int *a, int n) {
+    int max = 0;
     for (int i = 0; i < n; ++i) {
-        if(a[i]>max)
-            max=a[i];
+        if (a[i] > max)
+            max = a[i];
     }
     return max;
 }
 
-void sortRowsByMinElement(matrix m){
+void sortRowsByMinElement(matrix m) {
     insertionSortRowsMatrixByRowCriteria(&m, getMax);
 }
-void test_sortRowsMaxAndMinElement(){
-    matrix m1= createMatrixFromArray(
+
+void test_sortRowsMaxAndMinElement() {
+    matrix m1 = createMatrixFromArray(
             (int[]) {
-                    1,2,3,
-                    4,5,6,
-                    7,8,9
+                    1, 2, 3,
+                    4, 5, 6,
+                    7, 8, 9
 
 
             },
             3, 3
     );
 
-    matrix m2= createMatrixFromArray(
+    matrix m2 = createMatrixFromArray(
             (int[]) {
-                    7,8,9,
-                    4,5,6,
-                    1,2,3
+                    7, 8, 9,
+                    4, 5, 6,
+                    1, 2, 3
 
 
             },
@@ -70,49 +72,53 @@ void test_sortRowsMaxAndMinElement(){
     );
     int minRow = getMinElementRow(m1);
     int maxRow = getMaxElementRow(m1);
-    swapRows(&m1,minRow,maxRow);
-    assert(areTwoMatricesEqual(&m1,&m2));
+    swapRows(&m1, minRow, maxRow);
+    assert(areTwoMatricesEqual(&m1, &m2));
 }
-void test_sortRowsByMinElement(){
-    int a={1,2,3,4,5};
-    assert(getMax(&a,5));
-    matrix m1= createMatrixFromArray(
+
+void test_sortRowsByMinElement() {
+    int a = {1, 2, 3, 4, 5};
+    assert(getMax(&a, 5));
+    matrix m1 = createMatrixFromArray(
             (int[]) {
-                    7,1,2,
-                    1,8,1,
-                    3,2,3
+                    7, 1, 2,
+                    1, 8, 1,
+                    3, 2, 3
 
 
             },
             3, 3
     );
-    matrix m2= createMatrixFromArray(
+    matrix m2 = createMatrixFromArray(
             (int[]) {
-                    3,2,3,
-                    7,1,2,
-                    1,8,1
+                    3, 2, 3,
+                    7, 1, 2,
+                    1, 8, 1
 
 
             },
             3, 3
     );
     sortRowsByMinElement(m1);
-    assert(areTwoMatricesEqual(&m1,&m2));
+    assert(areTwoMatricesEqual(&m1, &m2));
 }
-int getMin(int *a, int n){
-    int min=a[0];
+
+int getMin(int *a, int n) {
+    int min = a[0];
     for (int i = 1; i < n; ++i) {
-        if(a[i]<min)
-            min=a[i];
+        if (a[i] < min)
+            min = a[i];
     }
     return min;
 }
-void sortColsByMinElement(matrix m){
-    selectionSortColsMatrixByColCriteria(&m,getMin);
+
+void sortColsByMinElement(matrix m) {
+    selectionSortColsMatrixByColCriteria(&m, getMin);
 }
-void test_sortColsByMinElement(){
-    int a={1,2,3,4,5};
-    assert(getMin(&a,5));
+
+void test_sortColsByMinElement() {
+    int a = {1, 2, 3, 4, 5};
+    assert(getMin(&a, 5));
     matrix m1 = createMatrixFromArray((int[]) {3, 5, 2, 1, 3, 1,
                                                2, 5, 1, 8, 1, 7,
                                                1, 1, 4, 1, 8, 3}, 3, 6);
@@ -141,20 +147,22 @@ matrix mulMatrices(matrix m1, matrix m2) {
         printf("\n");
     }
 }
-void getSquareOfMatrixIfSymmetric(matrix *m){
-    if(isSymmetricMatrix(m)==1)
-        mulMatrices(*m,*m);
+
+void getSquareOfMatrixIfSymmetric(matrix *m) {
+    if (isSymmetricMatrix(m) == 1)
+        mulMatrices(*m, *m);
     else
         outputMatrix(&m);
 }
-void test_getSquareOfMatrixIfSymmetric(){
-    matrix m1 = createMatrixFromArray((int[]) {1, 2, 3,
-                                              2, 5, 6,
-                                              3, 6, 9}, 3, 3);
 
-    matrix m2= createMatrixFromArray((int[]) {14, 30, 42,
-                                              30, 65, 90,
-                                              42, 90, 126}, 3, 3);
+void test_getSquareOfMatrixIfSymmetric() {
+    matrix m1 = createMatrixFromArray((int[]) {1, 2, 3,
+                                               2, 5, 6,
+                                               3, 6, 9}, 3, 3);
+
+    matrix m2 = createMatrixFromArray((int[]) {14, 30, 42,
+                                               30, 65, 90,
+                                               42, 90, 126}, 3, 3);
 
     getSquareOfMatrixIfSymmetric(&m1);
 
@@ -162,117 +170,126 @@ void test_getSquareOfMatrixIfSymmetric(){
 
 }
 
-bool isUnique(int *a,int n) {
+bool isUnique(int *a, int n) {
     for (int i = 0; i < n; ++i) {
-            if (a[i] == a[i+1]) {
-                return false;
+        if (a[i] == a[i + 1]) {
+            return false;
 
         }
 
     }
     return true;
 }
-int getSum(int *a,int n){
-    int sum=0;
+
+int getSum(int *a, int n) {
+    int sum = 0;
     for (int i = 0; i < n; ++i) {
         sum += a[i];
     }
     return sum;
 }
-void transposeIfMatrixHasNotEqualSumOfRows(matrix m){
-    int *sum=(int *)(malloc(sizeof(int)*m.nRows));
+
+void transposeIfMatrixHasNotEqualSumOfRows(matrix m) {
+    int *sum = (int *) (malloc(sizeof(int) * m.nRows));
     for (int i = 0; i < m.nRows; ++i)
-        sum[i]= getSum(m.values[i],m.nCols);
-    if(isUnique(sum,m.nRows)){
+        sum[i] = getSum(m.values[i], m.nCols);
+    if (isUnique(sum, m.nRows)) {
         transposeSquareMatrix(&m);
-    }else
+    } else
         outputMatrix(&m);
 }
-int test_transposeIfMatrixHasNotEqualSumOfRows(){
+
+int test_transposeIfMatrixHasNotEqualSumOfRows() {
     SetConsoleOutputCP(CP_UTF8);
     matrix m1 = createMatrixFromArray((int[]) {1, 2, 3,
                                                0, 1, 1,
                                                3, 6, 9}, 3, 3);
 
-    matrix m2= createMatrixFromArray((int[]) {1, 0, 3,
-                                              2, 1, 6,
-                                              3, 1, 9}, 3, 3);
-    int a[5]={1,2,3,4,5};
-    int result= getSum(&a,5);
-    assert(result==15);
-    int result1= isUnique(a,5);
-    assert(result1==1);
+    matrix m2 = createMatrixFromArray((int[]) {1, 0, 3,
+                                               2, 1, 6,
+                                               3, 1, 9}, 3, 3);
+    int a[5] = {1, 2, 3, 4, 5};
+    int result = getSum(&a, 5);
+    assert(result == 15);
+    int result1 = isUnique(a, 5);
+    assert(result1 == 1);
 
     transposeIfMatrixHasNotEqualSumOfRows(m1);
-    assert(areTwoMatricesEqual(&m1,&m2));
+    assert(areTwoMatricesEqual(&m1, &m2));
 }
 
-bool isMutuallyInverseMatrices(matrix m1, matrix m2){
-    matrix result1= mulMatrices(m1,m2);
-    matrix result2= mulMatrices(m2,m1);
-    bool E=isEMatrix(&result1)&& isEMatrix(&result2);
+bool isMutuallyInverseMatrices(matrix m1, matrix m2) {
+    matrix result1 = mulMatrices(m1, m2);
+    matrix result2 = mulMatrices(m2, m1);
+    bool E = isEMatrix(&result1) && isEMatrix(&result2);
     return E;
 }
-void test_isMutuallyInverseMatrices(){
+
+void test_isMutuallyInverseMatrices() {
     matrix m1 = createMatrixFromArray((int[]) {2, 0, 1,
                                                1, 5, -4,
                                                -1, 1, 0}, 3, 3);
 
-    matrix m2= createMatrixFromArray((int[]) {-4, 0, 3,
-                                              2, 1, 6,
-                                              3, 1, 9}, 3, 3);
+    matrix m2 = createMatrixFromArray((int[]) {-4, 0, 3,
+                                               2, 1, 6,
+                                               3, 1, 9}, 3, 3);
 
-    assert(!isMutuallyInverseMatrices(m1,m2));
+    assert(!isMutuallyInverseMatrices(m1, m2));
 }
-int max2(int a,int b){
-    if(a>b){
+
+int max2(int a, int b) {
+    if (a > b) {
         return a;
     }
     return b;
 }
-long long findSumOfMaxesOfPseudoDiagonal(matrix m){
-    int* max_value=(int*) malloc(sizeof(int)*(m.nRows+m.nCols-1));
-    for(int i=0;i<m.nRows+m.nCols-1;i++){
-        max_value[i]=0;
+
+long long findSumOfMaxesOfPseudoDiagonal(matrix m) {
+    int *max_value = (int *) malloc(sizeof(int) * (m.nRows + m.nCols - 1));
+    for (int i = 0; i < m.nRows + m.nCols - 1; i++) {
+        max_value[i] = 0;
     }
     for (int i = 0; i < m.nRows; ++i) {
         for (int j = 0; j < m.nCols; ++j) {
-             if(i!=j){
-                 max_value[i-j+m.nCols-1]=max(max_value[i-j+m.nCols-1],m.values[i][j]);
-             }
+            if (i != j) {
+                max_value[i - j + m.nCols - 1] = max(max_value[i - j + m.nCols - 1], m.values[i][j]);
+            }
         }
     }
-    long long sum=0;
-    for (int i = 0; i < m.nRows+m.nCols-1; ++i) {
+    long long sum = 0;
+    for (int i = 0; i < m.nRows + m.nCols - 1; ++i) {
         sum += max_value[i];
 
     }
     return sum;
 }
-void test_findSumOfMaxesOfPseudoDiagonal(){
-    matrix m1 = createMatrixFromArray((int[]) {3,2,5,4,
-                                               1, 3,6,3,
-                                               3,2,1,2}, 3, 4);
+
+void test_findSumOfMaxesOfPseudoDiagonal() {
+    matrix m1 = createMatrixFromArray((int[]) {3, 2, 5, 4,
+                                               1, 3, 6, 3,
+                                               3, 2, 1, 2}, 3, 4);
 
 
-    int a=3;
-    int b=5;
-    int result= max2(a,b);
-    assert(result==5);
-    int result1= findSumOfMaxesOfPseudoDiagonal(m1);
-    assert(result1=20);
+    int a = 3;
+    int b = 5;
+    int result = max2(a, b);
+    assert(result == 5);
+    int result1 = findSumOfMaxesOfPseudoDiagonal(m1);
+    assert(result1 = 20);
 }
-bool value_is_in_the_area(position posit,int i,int j){
-    position new={i-posit.rowIndex,j-posit.rowIndex};
-    return (new.rowIndex<=new.colIndex);
+
+bool value_is_in_the_area(position posit, int i, int j) {
+    position new = {i - posit.rowIndex, j - posit.rowIndex};
+    return (new.rowIndex <= new.colIndex);
 }
-int getMinInArea(matrix m){
-    position max= getMaxValuePos(m);
-    int min=INT_MAX;
+
+int getMinInArea(matrix m) {
+    position max = getMaxValuePos(m);
+    int min = INT_MAX;
 
     for (int i = 0; i <= max.rowIndex; i++) {
         for (int j = 0; j < m.nCols; j++) {
-            if (value_is_in_the_area(max,i,j) && m.values[i][j] < min) {
+            if (value_is_in_the_area(max, i, j) && m.values[i][j] < min) {
                 min = m.values[i][j];
             }
         }
@@ -280,72 +297,77 @@ int getMinInArea(matrix m){
 
     return min;
 }
-void test_getMinInArea(){
-    matrix m1 = createMatrixFromArray((int[]) {10,7,5,6,
-                                               3,11,8,9,
-                                               4,1,12,2}, 3, 4);
+
+void test_getMinInArea() {
+    matrix m1 = createMatrixFromArray((int[]) {10, 7, 5, 6,
+                                               3, 11, 8, 9,
+                                               4, 1, 12, 2}, 3, 4);
 
 
-    position posit={3,2};
-    int result= value_is_in_the_area(posit,3,3);
-    assert(result==1);
-    int result1= getMinInArea(m1);
-    assert(result1==2);
+    position posit = {3, 2};
+    int result = value_is_in_the_area(posit, 3, 3);
+    assert(result == 1);
+    int result1 = getMinInArea(m1);
+    assert(result1 == 2);
 }
-float getDistance(int *a, int n){
-    float d=0;
+
+float getDistance(int *a, int n) {
+    float d = 0;
     for (int i = 0; i < n; ++i) {
-        d+= sqrt(pow(a[i],2));
+        d += sqrt(pow(a[i], 2));
     }
     return d;
 }
-void sortByDistances(matrix m){
+
+void sortByDistances(matrix m) {
     insertionSortRowsMatrixByRowCriteria1(&m, getDistance);
 }
-void test_sortByDistances(){
 
-    matrix m1 = createMatrixFromArray((int[]) {10,7,5,6,
-                                               3,11,8,9,
-                                               4,1,12,2}, 3, 4);
+void test_sortByDistances() {
 
-    matrix m2= createMatrixFromArray((int[]) {3,11,8,9,
-                                              10,7,5,6,
-                                              3,11,8,9
+    matrix m1 = createMatrixFromArray((int[]) {10, 7, 5, 6,
+                                               3, 11, 8, 9,
+                                               4, 1, 12, 2}, 3, 4);
+
+    matrix m2 = createMatrixFromArray((int[]) {3, 11, 8, 9,
+                                               10, 7, 5, 6,
+                                               3, 11, 8, 9
     }, 3, 4);
-    int a[5]={1,1,1,1,1};
-    float result= getDistance(a,5);
-    assert(result=2.23606797749979);
+    int a[5] = {1, 1, 1, 1, 1};
+    float result = getDistance(a, 5);
+    assert(result = 2.23606797749979);
     sortByDistances(m1);
-    assert(areTwoMatricesEqual(&m1,&m2));
+    assert(areTwoMatricesEqual(&m1, &m2));
 }
 
-int cmp_long_long(const void *pa, const void *pb){
-    return *(long long *)pa-*(long long *)pb;
+int cmp_long_long(const void *pa, const void *pb) {
+    return *(long long *) pa - *(long long *) pb;
 }
-int countNUnique(long long *a, int n){
-    qsort(a,n,sizeof(long long),cmp_long_long);
-    int count_unique=n==0 ? 1:0;
-    int i=0;
-    while(i<n){
-        int left=0;
-        int right=n-1;
-        while(left<=right){
-            int middle=left+(right-left)/2;
-            if(a[middle]==a[i]){
-            i=middle;
-            left=middle+1;
-            }
-            else if(a[middle]>a[i])
-                right=middle-1;
+
+int countNUnique(long long *a, int n) {
+    qsort(a, n, sizeof(long long), cmp_long_long);
+    int count_unique = n == 0 ? 1 : 0;
+    int i = 0;
+    while (i < n) {
+        int left = 0;
+        int right = n - 1;
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+            if (a[middle] == a[i]) {
+                i = middle;
+                left = middle + 1;
+            } else if (a[middle] > a[i])
+                right = middle - 1;
             else
-                left=middle+1;
+                left = middle + 1;
         }
         i++;
         count_unique++;
     }
     return count_unique;
 }
-int countEqClassesByRowsSum(matrix m){
+
+int countEqClassesByRowsSum(matrix m) {
     long long *sum = (long long *) malloc(sizeof(long long) * m.nRows);
     for (int i = 0; i < m.nRows; i++)
         sum[i] = getSum(m.values[i], m.nCols);
@@ -353,38 +375,59 @@ int countEqClassesByRowsSum(matrix m){
     free(sum);
     return count_of_unique;
 }
-void test_countEqClassesByRowsSum(){
-    matrix m1 = createMatrixFromArray((int[]) {7,1,
-                                               2,7,
-                                               5,4,
-                                               4,3,
-                                               1,6,
-                                               8,0
+
+void test_countEqClassesByRowsSum() {
+    matrix m1 = createMatrixFromArray((int[]) {7, 1,
+                                               2, 7,
+                                               5, 4,
+                                               4, 3,
+                                               1, 6,
+                                               8, 0
     }, 6, 2);
 
-    int a[5]={1,2,3,1,1};
-    float result= countNUnique(a,5);
-    assert(result==5);
-    int result1= countEqClassesByRowsSum(m1);
-    assert(result1==3);
+    int a[5] = {1, 2, 3, 1, 1};
+    float result = countNUnique(a, 5);
+    assert(result == 5);
+    int result1 = countEqClassesByRowsSum(m1);
+    assert(result1 == 3);
 }
-int main(){
 
-    matrix m1 = createMatrixFromArray((int[]) {7,1,
-                                               2,7,
-                                               5,4,
-                                               4,3,
-                                               1,6,
-                                               8,0
-                                               }, 6, 2);
+int getNSpecialElement(matrix m) {
+    int count = 0;
+    int sum = 0;
+    for (int j = 0; j < m.nCols; ++j) {
+        for (int i = 0; i < m.nRows; ++i) {
+            sum += m.values[i][j];
+        }
+        for (int z = 0; z < m.nRows; ++z) {
+            if (m.values[z][j] > sum - m.values[z][j])
+                count++;
+        }
+        sum = 0;
+    }
 
-    matrix m2= createMatrixFromArray((int[]) {3,11,8,9,
-                                              10,7,5,6,
-                                              3,11,8,9
-                                              }, 3, 4);
-    int a[5]={1,2,3,1,1};
-    float result= countNUnique(a,5);
-    assert(result==5);
-    int result1= countEqClassesByRowsSum(m1);
-    assert(result1==3);
+    return count;
+}
+void test_getNSpecialElement(){
+    matrix m1 = createMatrixFromArray((int[]) {3, 5, 5, 4,
+                                               2, 3, 6, 7,
+                                               12, 2, 1, 2
+    }, 3, 4);
+
+
+    int result = getNSpecialElement(m1);
+
+    assert(result == 2);
+}
+int main() {
+
+    matrix m1 = createMatrixFromArray((int[]) {3, 5, 5, 4,
+                                               2, 3, 6, 7,
+                                               12, 2, 1, 2
+    }, 3, 4);
+
+
+    int result = getNSpecialElement(m1);
+
+    assert(result == 2);
 }
