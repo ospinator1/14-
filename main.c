7b69b15f4cc43d8e6baeddfcf9e38fec761569e3
Stas <loktiost@gmail.com>
2024-04-07 16:02:40 +0300

    1 задача-тесты

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include "libs/strings/string/string_.h"
 #include <windows.h>
+#include <assert.h>
+
+
+
+void test_findLength(){
+    char str[] = "Hello";
+    char ptr = findLength(str);
+    printf("%d",ptr);
+}
 int main() {
-    SetConsoleOutputCP(CP_UTF8);
+    test_findLength();
 }
