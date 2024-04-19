49c0225e53fec5c0c231ef56e4a9a75210f23b1c
Stas <loktiost@gmail.com>
2024-04-19 12:11:39 +0300

    10 задача-тесты

--- libs/strings/string/tasks/tasks10/tasks10.h
+++ libs/strings/string/tasks/tasks10/tasks10.h
@@ -2,30 +2,56 @@
 
 #ifndef UNTITLED7_TASKS10_H
 #define UNTITLED7_TASKS10_H
-void reverseWords(char *string){
+
+#include "../../string_.h"
+
+#define ASSERT_STRING(expected, got) assertString(expected, got, \
+__FILE__, __FUNCTION__, __LINE__)
+
+void reverseWords(char *str) {
     char buffer[100];
-    int i,j,k;
-    for ( i = 0; string[i]!='\0'; ++i) {
-        buffer[i]=string[i];
+    int i, j, k;
+    for (i = 0; str[i] != '\0'; i++) {
+        buffer[i] = str[i];
     }
-    buffer[i]='\0';
-    i=i-1;
-    k=0;
-    while(i>=0) {
-        while (i >= 0 && buffer[i] != " ") {
+    buffer[i] = '\0';
+
+    i = i - 1;
+    k = 0;
+    while (i >= 0) {
+        while (i >= 0 && buffer[i] != ' ') {
             i--;
         }
         j = i + 1;
-        while(buffer[j] != '\0' && buffer[j] != ' ') {
-            string[k++] = buffer[j++];
+        while (buffer[j] != '\0' && buffer[j] != ' ') {
+            str[k++] = buffer[j++];
         }
-
         if (i >= 0) {
-            string[k++] = ' ';
-
+            str[k++] = ' ';
         }
+
         i--;
     }
-    string[k]='\0';
+    str[k] = '\0';
+}
+void test_reverseWords1(){
+    char s[]="";
+    reverseWords(s);
+    ASSERT_STRING("",s);
+}
+void test_reverseWords2(){
+    char s[]="a b c d";
+    reverseWords(s);
+    ASSERT_STRING("d c b a",s);
+}
+void test_reverseWords3() {
+    char s[] = "herald of darkness";
+    reverseWords(s);
+    ASSERT_STRING("darkness of herald", s);
+}
+void test_reverseWords(){
+    test_reverseWords1();
+    test_reverseWords2();
+    test_reverseWords3();
 }
 #endif //UNTITLED7_TASKS10_H
--- main.c
+++ main.c
@@ -1,10 +1,12 @@
-
 #include "libs/strings/string/tasks/tasks9/tasks9.h"
+#include "libs/strings/string/tasks/tasks10/tasks10.h"
 #include "libs/strings/string/string_.h"
 
-
+#include <windows.h>
 
 int main() {
-    test_combiningTwoStrings();
+    SetConsoleOutputCP(CP_UTF8);
+    test_reverseWords();
     return 0;
 }
+
