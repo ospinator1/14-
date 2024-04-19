0418828973bb16680ef051c426acc058d06424fe
Stas <loktiost@gmail.com>
2024-04-19 13:10:03 +0300

    11 задача-тесты

--- libs/strings/string/tasks/tasks11/tasks11.h
+++ libs/strings/string/tasks/tasks11/tasks11.h
@@ -7,61 +7,73 @@
 
 #include <stdbool.h>
 #include "../../string_.h"
+#include <assert.h>
 
-typedef enum WordBeforeFirstWordWithAReturnCode {
-    FIRST_WORD_WITH_A,
+#define ASSERT_STRING(expected, got) assertString(expected, got, \
+__FILE__, __FUNCTION__, __LINE__)
+#define MAX_WORD_SIZE 20
+typedef enum word_before_first_word_with_a_return_code {
+    EMPTY_STRING,
     NOT_FOUND_A_WORD_WITH_A,
-    WORD_FOUND,
-    EMPTY_STRING
-} WordBeforeFirstWordWithAReturnCode;
+    FIRST_WORD_WITH_A,
+    WORD_FOUND
+} word_before_first_word_with_a_return_code;
 
-typedef struct WordDescriptor {
-    char *begin; // позиция начала слова
-    char *end; // позиция первого символа, после последнего символа
-
-} WordDescriptor;
-
-int getWord(char *beginSearch, WordDescriptor *word) {
-    word->begin = findNonSpace(beginSearch);
-    if (*word->begin == '\0')
-        return 0;
-    word->end = findSpace(word->begin);
-    return 1;
-}
 
 bool isLettersInWord(WordDescriptor word, char letters) {
-    char *start = word.begin;
-    while (start != word.begin) {
+    char* start = word.begin;
+
+    while (start != word.end) {
         if (*start == letters)
             return true;
+
         start++;
     }
+
     return false;
 }
 
-int get_word_before_first_word_a(char *s, WordDescriptor *word) {
-    char *beginSearch = s;
+
+word_before_first_word_with_a_return_code getWordBeforeFirstWordWithA(char* s, WordDescriptor * word) {
+    char* begin_search = s;
     WordDescriptor word1;
-    if (!getWord(beginSearch, &word1)) {
+    if (!getWord(begin_search, &word1)) {
         return EMPTY_STRING;
     }
-    beginSearch = word1.end + 1;
+    begin_search = word1.end + 1;
     if (isLettersInWord(word1, 'a') || isLettersInWord(word1, 'A')) {
         return FIRST_WORD_WITH_A;
     }
     WordDescriptor word2;
-    while (getWord(beginSearch, &word2)) {
+    while (getWord(begin_search, &word2)) {
         if (isLettersInWord(word2, 'a') || isLettersInWord(word2, 'A')) {
             *word = word1;
             return WORD_FOUND;
         }
-
         word1 = word2;
-        beginSearch = word2.end + 1;
-
+        begin_search = word2.end + 1;
     }
     return NOT_FOUND_A_WORD_WITH_A;
 }
+
+
+void test_11_all_test_get_before_first_word_with_a() {
+    WordDescriptor word;
+
+    char s1[] = "";
+    assert(getWordBeforeFirstWordWithA(s1, &word)== EMPTY_STRING);
+    char s2[] = "ABC";
+    assert(getWordBeforeFirstWordWithA(s2, &word)== FIRST_WORD_WITH_A);
+    char s3[] = "BC A";
+    assert(getWordBeforeFirstWordWithA(s3, &word)== WORD_FOUND);
+    char got[MAX_WORD_SIZE];
+    copy(word.begin, word.end, got);
+    got[word.end - word.begin] = '\0';
+    ASSERT_STRING("BC", got);
+    char s4[] = "B Q WE YR OW IUWR";
+    assert(getWordBeforeFirstWordWithA(s4, &word) ==NOT_FOUND_A_WORD_WITH_A);
+}
+
 
 
 
--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include "libs/strings/string/tasks/tasks9/tasks9.h"
-#include "libs/strings/string/tasks/tasks10/tasks10.h"
+#include "libs/strings/string/tasks/tasks11/tasks11.h"
 #include "libs/strings/string/string_.h"
 
 #include <windows.h>
 
 int main() {
     SetConsoleOutputCP(CP_UTF8);
-    test_reverseWords();
+    test_11_all_test_get_before_first_word_with_a();
     return 0;
 }
 
