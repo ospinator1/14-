4c38f81bcd27e56c87a4d8bb6736ff39c0ce60ea
Stas <loktiost@gmail.com>
2024-05-17 15:23:14 +0300

    2 задача

---
+++ libs/generalized tasks of the semester/tasks1.h
@@ -0,0 +1,77 @@
+//
+// Created by Assa on 17.05.2024.
+//
+
+#ifndef UNTITLED7_TASKS1_H
+#define UNTITLED7_TASKS1_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <assert.h>
+
+#include "..//data_structures/matrix/matrix.h"
+
+typedef struct coord {
+    int row1;
+    int col1;
+    int row2;
+    int col2;
+} coord;
+
+
+void fill_matrix(const char* filename) {
+    FILE* file = fopen(filename, "rb");
+    int n;
+    fread(&n, sizeof(int), 1, file);
+    matrix m = getMemMatrix(n, n);
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            m.values[i][j] = 0;
+    coord c;
+    while(fread(&c, sizeof(coord), 1, file) == 1)
+        for (int i = c.row1; i <= c.row2; i++)
+            for (int j = c.col1; j <= c.col2; j++)
+                m.values[i][j]++;
+
+    fclose(file);
+    file = fopen(filename, "wb");
+    fwrite(&n, sizeof(int), 1, file);
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            fwrite(&m.values[i][j], sizeof(int), 1, file);
+    fclose(file);
+}
+
+void test_fill_matrix() {
+    const char* filename = "C:\\Users\\Assa\\CLionProjects\\untitled7\\text labs 19\\1 tasks\\1_2.txt";
+
+    FILE* file = fopen(filename, "wb");
+
+    int n = 3;
+    fwrite(&n, sizeof(int), 1, file);
+
+    coord c1 = {1, 1, 2, 2};
+    coord c2 = {0, 0, 1, 1};
+    fwrite(&c1, sizeof(coord), 1, file);
+    fwrite(&c2, sizeof(coord), 1, file);
+    fclose(file);
+    fill_matrix(filename);
+    file = fopen(filename, "rb");
+    int matrix_values[n][n];
+    fread(&n, sizeof(int), 1, file);
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            fread(&matrix_values[i][j], sizeof(int), 1, file);
+    fclose(file);
+    printf("Updated matrix:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", matrix_values[i][j]);
+        }
+        printf("\n");
+    }
+}
+int main(){
+    test_fill_matrix();
+}
+#endif //UNTITLED7_TASKS1_H
--- main.c
+++ main.c
@@ -1,72 +1,57 @@
 #include <stdio.h>
-#include "libs/data_structures//matrix/matrix.h"
+#define c 3
+#define r 4
 
-typedef struct coord {
-    int row1;
-    int col1;
-    int row2;
-    int col2;
-} coord;
+int count_Live_neighbors(int board[c][r], int row, int col) {
+    int count = 0;
+    for (int i = row - 1; i <= row + 1; i++) {
+        for (int j = col - 1; j <= col + 1; j++) {
+            if (i >= 0 && i < c && j >= 0 && j < r && !(i == row && j == col)) {
+                count += board[i][j];
+            }
+        }
+    }
 
-
-void fill_matrix(const char* filename) {
-    FILE* file = fopen(filename, "rb");
-    int n;
-    fread(&n, sizeof(int), 1, file);
-    matrix m = getMemMatrix(n, n);
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            m.values[i][j] = 0;
-    coord c;
-    while(fread(&c, sizeof(coord), 1, file) == 1)
-        for (int i = c.row1; i <= c.row2; i++)
-            for (int j = c.col1; j <= c.col2; j++)
-                m.values[i][j]++;
-
-    fclose(file);
-    file = fopen(filename, "wb");
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            fwrite(&m.values[i][j], sizeof(int), 1, file);
-    fclose(file);
+    return count;
 }
 
-void test_fill_matrix() {
-    const char* filename = "C:\\Users\\Assa\\CLionProjects\\untitled7\\text labs 19\\1 tasks\\1_2.txt";
+void game_life(int board[c][r]) {
+    int nextBoard[c][r];
+    for (int i = 0; i < c; ++i) {
+        for (int j = 0; j < r; ++j) {
+            int liveNeighbors = count_Live_neighbors(board, i, j);
 
-    FILE* file = fopen(filename, "wb");
-
-    int n = 3;
-    fwrite(&n, sizeof(int), 1, file);
-
-    coord c1 = {1, 1, 2, 2};
-    coord c2 = {0, 0, 1, 1};
-    fwrite(&c1, sizeof(coord), 1, file);
-    fwrite(&c2, sizeof(coord), 1, file);
-
-    fclose(file);
-
-    fill_matrix(filename);
-
-    file = fopen(filename, "rb");
-
-    int matrix_values[n][n];
-    fread(&n, sizeof(int), 1, file);
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            fread(&matrix_values[i][j], sizeof(int), 1, file);
-
-    fclose(file);
-
-
-    printf("Updated matrix:\n");
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            printf("%d ", matrix_values[i][j]);
+            if (board[i][j] == 1) {
+                if (liveNeighbors < 2 || liveNeighbors > 3) {
+                    nextBoard[i][j] = 0;
+                } else {
+                    nextBoard[i][j] = 1;
+                }
+            } else {
+                if (liveNeighbors == 3) {
+                    nextBoard[i][j] = 1;
+                } else {
+                    nextBoard[i][j] = 0;
+                }
+            }
         }
-        printf("\n");
+    }
+    for (int i = 0; i < c; i++) {
+        for (int j = 0; j < r; j++) {
+            board[i][j] = nextBoard[i][j];
+        }
     }
 }
-int main(){
-    test_fill_matrix();
+void test() {
+    int board[c][r] = {{0, 1, 0},{0, 0, 1},{1, 1, 1},{0, 0, 0}};
+    game_life(board);
+    int check[c][r] = {{0, 0, 0},{1, 0, 1},{0, 1, 1},{0, 1, 0}};
+    for (int i = 0; i < c; i++)
+        for (int j = 0; j < r; j++)
+            if (board[i][j] != check[i][j])
+                printf("Matrices are different\n");
+
+}
+int main() {
+    test();
 }
