1fbcccadd1fd9499acfbf8cd8fd70faad88a88f5
Stas <loktiost@gmail.com>
2024-05-19 09:45:22 +0300

    10 задача

--- main.c
+++ main.c
@@ -1,25 +1,28 @@
+#include <stdio.h>
 #include <stdlib.h>
-#include "libs/generalized tasks of the semester/tasks9.h"
-#include <time.h>
-void test(){
-    int n = 500;
-    srand((unsigned) time(NULL));
-    FILE *fp = fopen ("C:\\Users\\Assa\\CLionProjects\\untitled7\\text labs 20\\tasks9\\1.txt", "w+");
-
-    int expArray[1000];
-    int idx = 0;
-    for (int i = 0; i < 1000; i++) {
-        int val = rand();
-        if (val < n) {
-            expArray[idx++] = val;
-        }
-        fprintf(fp, "%d\n", val);
-    }
-    fclose(fp);
-
-    size_t res = fileWithSmallerValues("C:\\Users\\Assa\\CLionProjects\\untitled7\\text labs 20\\tasks9\\1.txt", "C:\\Users\\Assa\\CLionProjects\\untitled7\\text labs 20\\tasks9\\2.txt", n);
-    assert(idx == res);
+#include <signal.h>
+FILE *file;
+int N;
+void handle_sigint(int sig) {
+    char line[256];
+    for (int i = 0; i < N; i++)
+        if(fgets(line, sizeof(line), file))
+            printf("%s", line);
+        else
+            exit(0);
 }
-int main() {
- test();
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Usage: %s <filename> <N>\n", argv[0]);
+        return 1;
+    }
+    file = fopen(argv[1], "r");
+    if (file == NULL) {
+        perror("Error opening file");
+        return 1;
+    }
+    N = atoi(argv[2]);
+    signal(SIGINT, handle_sigint);
+    getchar();
+    return 0;
 }
